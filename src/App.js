b1e177119a1efe9c4c918035f112bb858169d4a8
fangnx <fangnaxin@gmail.com>
2019-05-04 21:53:42 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -16,4 +16,9 @@ class App extends Component {
   }
 }
 
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
 export default App;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
-class Post extends Component {
+import React from "react";
+import { Container, Label, Image, Grid } from "semantic-ui-react";
+
+class Post extends React.Component {
   render() {
     return (
-      <article className="Post" ref="Post">
-        <header>
-          <div className="Post-user">
-            <div className="Post-user-avatar">
-              <img
-                src="https://www.laravelnigeria.com/img/chris.jpg"
-                alt="Chris"
-              />
-            </div>
-            <div className="Post-user-nickname">
-              <span>Chris</span>
-            </div>
-          </div>
-        </header>
-        <div className="Post-image">
-          <div className="Post-image-bg">
-            <img
-              alt="Icon Living"
-              src="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
+      // <Container fluid>
+        <Grid>
+          <Grid.Row width={2}>
+            <Label as="a" color="blue" size="large" image>
+              <img src="https://react.semantic-ui.com/images/avatar/small/veronika.jpg" />
+              <span>
+                Claire He &nbsp;&nbsp;
+                <i class="france flag" />
+              </span>
+              <Label.Detail>User</Label.Detail>
+            </Label>
+          </Grid.Row>
+
+          <Grid.Row width={14}> 
+            <Image
+              src="https://images.unsplash.com/photo-1555508275-b5451b4c240a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+              size="large"
             />
-          </div>
-        </div>
-        <div className="Post-caption">
-          <strong>Chris</strong> Moving the community!
-        </div>
-      </article>
+          </Grid.Row>
+        </Grid>
+      // </Container>
     );
   }
 }
+
 export default Post;
