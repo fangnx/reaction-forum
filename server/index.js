7eb30725ef7ac15bf8980d9ae24f392ddfd8f090
fangnx <fangnaxin@gmail.com>
2019-05-12 23:22:10 -0400

    Updated config

--- server/index.js
+++ server/index.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
 const app = express();
+const port = 3000;
+
+// loads .env file
+require('dotenv').config();
+
+app.use(
+  session({
+    secret: process.env.SECRET_WORD,
+    resave: true, // forces updating session
+    saveUninitialized: false // initializes a session even if not logged in
+  })
+);
+
+const oidc = new ExpressOIDC({
+  issuer: process.env.OKTA_ORG_URL,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  appBaseUrl: process.env.OKTA_BASE_URL,
+  redirect_URI: process.env.REDIRECT_URL,
+  scope: 'openid profile',
+  routes: {
+    callback: {
+      path: '/authorization-code/callback',
+      defaultRedirect: '/admin'
+    }
+  }
+});
+
+app.use(oidc.router);
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.send('hello, express');
+  res.send('<p><h1>Welcome to Express!<h1><p>');
 });
 
-app.get('/users/:name', function(req, res) {
-  res.send('hello, ' + req.params.name);
-});
-
-app.listen(3000);
+app.listen(port, () => console.log(`App is listening on port ${port}~~~`));
