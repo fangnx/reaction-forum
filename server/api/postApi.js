380d4cdd9bb7ced28de5d08ed6b5db9f94cb7097
fangnx <fangnaxin@gmail.com>
2019-06-10 22:08:09 -0400

    Add Post api & UI init

---
+++ server/api/postApi.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import keys from '../../config/config';
+import Post from '../../models/User';
+
+const router = express.Router();
+
+// Add a Post endpoint
+router.post('/add', (req, res) => {
+  // TODO: validation
+  const newPost = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    timeStamp: req.body.timeStamp,
+    tags: req.body.tags,
+    viewCount: req.body.viewCount,
+    likeCount: req.body.likeCount
+  });
+
+  newPost
+    .save()
+    .then(post => res.json(post))
+    .catch(err => console.log(err));
+});
