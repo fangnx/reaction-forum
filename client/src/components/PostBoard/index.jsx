e1ffdbd7f3cd3e087391ea48c65bb95ac5884749
fangnx <fangnaxin@gmail.com>
2019-06-15 01:01:38 -0400

    Updated PostView

--- client/src/components/PostBoard/index.jsx
+++ client/src/components/PostBoard/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-  Image,
-  Label,
-  Card,
-  Form,
-  TextArea,
-  Input,
-  Button,
-  Segment,
-  Icon
+	Image,
+	Label,
+	Card,
+	Form,
+	TextArea,
+	Input,
+	Button,
+	Segment,
+	Icon
 } from 'semantic-ui-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './PostBoard.css';
@@ -16,39 +16,43 @@ import { getAllPosts } from '../../actions/postService';
 import PostView from '../PostView';
 
 class PostBoard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: [] };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { posts: [] };
+	}
 
-  componentDidMount() {
-    getAllPosts().then(res => {
-      console.log(res.data);
-      if (res.data) {
-        this.setState({ posts: res.data.map(post => post) });
-      }
-    });
-  }
+	componentDidMount() {
+		getAllPosts().then(res => {
+			console.log(res.data);
+			if (res.data) {
+				this.setState({ posts: res.data.map(post => post) });
+			}
+		});
+	}
 
-  render() {
-    const { posts } = this.state;
+	render() {
+		const { posts } = this.state;
 
-    return (
-      <div className="postView-wrapper">
-        {posts.map(post => (
-          <PostView
-            title={post.title}
-            author={post.author}
-            content={post.content}
-            tags={post.tags}
-            timeStamp={post.timeStamp}
-            viewCount={post.viewCount}
-            likeCount={post.likeCount}
-          />
-        ))}
-      </div>
-    );
-  }
+		return (
+			<div className="postBoard-wrapper">
+				{posts.map(post => (
+					<React.Fragment>
+						<PostView
+							className="postBoard-postView"
+							title={post.title}
+							author={post.author}
+							content={post.content}
+							tags={post.tags}
+							timeStamp={post.timeStamp}
+							viewCount={post.viewCount}
+							likeCount={post.likeCount}
+						/>
+						<div className="postBoard-separator" />
+					</React.Fragment>
+				))}
+			</div>
+		);
+	}
 }
 
 export default PostBoard;
--- client/src/components/PostView/index.jsx
+++ client/src/components/PostView/index.jsx
@@ -1,39 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, Label, Card, Grid } from 'semantic-ui-react';
+import { Image, Label, Card, Grid, Divider, Header } from 'semantic-ui-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../App.css';
 import './PostView.css';
 
-class PostView extends React.Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div className="postView-wrapper">
-        <Card className="postView-card" fluid>
-          <Card.Header className="post-card-header">
-            <Label as="a" size="large" image>
-              <Image src="https://react.semantic-ui.com/images/avatar/small/veronika.jpg" />
-              <span>
-                {this.props.author} &nbsp;&nbsp;
-                {/* <i class="france flag" /> */}
-              </span>
-            </Label>
-          </Card.Header>
+const tagColors = [
+	'orange',
+	'yellow',
+	'olive',
+	'green',
+	'teal',
+	'blue',
+	'purple',
+	'brown',
+	'grey'
+];
 
-          <Card.Content className="postView-card-content">
-            <Grid padded className="postView-card-grid">
-              <Grid.Row>{this.props.title}</Grid.Row>
-              <Grid.Row>{this.props.content}}</Grid.Row>
-              <Grid.Row>{this.props.likeCount}</Grid.Row>
-            </Grid>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
+class PostView extends React.Component {
+	capitalizeTag(word) {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	}
+
+	render() {
+		console.log(this.props);
+		return (
+			<div className="postView-wrapper">
+				<Card className="postView-card" fluid>
+					<Card.Header className="postView-card-header">
+						<Label as="a" size="large" image>
+							<Image src="https://react.semantic-ui.com/images/avatar/small/veronika.jpg" />
+							<span>
+								{this.props.author} &nbsp;&nbsp;
+								{/* <i class="france flag" /> */}
+							</span>
+						</Label>
+					</Card.Header>
+
+					<Card.Content className="postView-card-content">
+						<Grid padded className="postView-card-grid">
+							<Grid.Row className="postView-title-field">
+								{this.props.title}
+							</Grid.Row>
+							<Grid.Row className="postView-content-field">
+								{this.props.content}}
+							</Grid.Row>
+						</Grid>
+
+						<Grid.Row className="postView-tags-field">
+							<Label.Group size="medium" className="postView-tagList">
+								{this.props.tags.map((tag, index) => (
+									<Label
+										className="postView-tag"
+										color={tagColors[index % tagColors.length]}
+									>
+										{this.capitalizeTag(tag)}
+									</Label>
+								))}
+							</Label.Group>
+							<Divider hidden />
+						</Grid.Row>
+
+						{/* <Grid.Row>
+							<span className="postView-viewCount-field">
+								<label>Viewed: {this.props.viewCount}</label>
+							</span>
+							<span className="postView-likeCount-field">
+								<label>Liked: {this.props.likeCount}</label>
+							</span>
+						</Grid.Row> */}
+					</Card.Content>
+				</Card>
+			</div>
+		);
+	}
 }
 export default PostView;
 
 PostView.propTypes = {
-  title: String
+	title: PropTypes.string
 };
