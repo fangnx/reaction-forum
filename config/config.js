ac946de254d50de3befbcd02c18dca94f73bbc8a
fangnx <fangnaxin@gmail.com>
2019-07-01 17:53:28 -0400

    Modified package.jsons

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || 5000,
-  session: {
-    secret: 'myblog',
-    key: 'myblog',
-    maxAge: 2592000000
-  },
-  mongodb: 'mongodb://localhost:27017/myblog',
-  secretOrKey: 'secret'
+	host: process.env.HOST || '127.0.0.1',
+	port: process.env.PORT || 5000,
+	session: {
+		secret: 'myblog',
+		key: 'myblog',
+		maxAge: 2592000000
+	},
+	mongodb: 'mongodb://localhost:27017/myblog',
+	secretOrKey: 'secret'
 };
